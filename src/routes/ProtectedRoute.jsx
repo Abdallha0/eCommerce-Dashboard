f77bc572588822d7e14a3289c54b553d85d3a315
Omar <wearethe1u@gmail.com>
2026-07-05 14:25:57 +0300

    fix structure

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,15 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "./context/AuthContext.jsx";
+import { useAuth } from "../context/AuthContext.jsx";
 
 function ProtectedRoute() {
   const token = localStorage.getItem("token");
-  const {user} = useAuth()
+  const { user } = useAuth();
   const isAuthenticated = !!token;
   const isAdmin = user?.role === "admin";
 
   if (!isAuthenticated) {
     return <Navigate replace to="/login" />;
   }
+
   if (!isAdmin) {
     return <Navigate replace to="/" />;
   }
