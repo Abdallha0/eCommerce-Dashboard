6d047e8ee4a02c04c7fc8100ddb0ca6e5fbf34e4
mazin189 <mazen.ahmed.abdelattii@gmail.com>
2026-07-14 23:00:52 +0300

    adding some styles

--- src/components/orders/header.jsx
+++ src/components/orders/header.jsx
@@ -1,3 +1,4 @@
+import { ChevronDown, Search } from 'lucide-react'
 import React from 'react'
 
 function OrdersHeader() {
@@ -18,11 +19,80 @@ function OrdersHeader() {
 
 
 
+<div className='flex items-center gap-3'>
+<div className='relative min-w-45 flex-1'>
+<span className="absolute -translate-y-1/2 top-1/2 left-3">
+<Search className="text-gray-400" size={18} />
+</span>
+<input 
+type="search"
+placeholder='Search ID, customer...'
+className='w-full rounded-lg border border-slate-200 h-9 bg-white pl-9 pr-3 text-sm text-slate-800 placeholder-slate-400 transition focus:border-slate-400 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-100'
+/>
+</div>
+
+               <div className="relative">
+                <select
+                className="appearance-none pl-4 dark:bg-slate-950 bg-slate-50 border dark:border-gray-800 border-slate-200 rounded-xl w-full py-2 px-8 outline-none">
+                <option value="electronics">All statuses</option>
+                <option value="phones">Pending</option>
+                <option value="fashion">Confirmed</option>
+                <option value="home">Processing</option>
+                <option value="beauty">Shipped</option>
+                <option value="sports">Delivered</option>
+                <option value="sports">Cancelled</option>
+                <option value="sports">Returned</option>
+                </select>
+                <ChevronDown
+                className="absolute right-2 top-1/2 -translate-y-1/2 pointer-events-none"
+                size={17}
+                />
+               </div>
 
 
 
 
 
+
+
+
+                <div className="relative">
+                <select
+                className="appearance-none pl-4 dark:bg-slate-950 bg-slate-50 border dark:border-gray-800 border-slate-200 rounded-xl w-full py-2 px-8 outline-none">
+                <option value="electronics">All payments</option>
+                <option value="phones">Pending</option>
+                <option value="fashion">Paid</option>
+                <option value="home">Failed</option>
+                </select>
+                <ChevronDown
+                className="absolute right-2 top-1/2 -translate-y-1/2 pointer-events-none"
+                size={17}
+                />
+               </div>
+
+
+
+
+
+
+
+                <div className="relative">
+                <select
+                className="appearance-none pl-4 dark:bg-slate-950 bg-slate-50 border dark:border-gray-800 border-slate-200 rounded-xl w-full py-2 px-8 outline-none">
+                <option value="electronics">All methods</option>
+                <option value="phones">Cash</option>
+                <option value="fashion">Stripe</option>
+                </select>
+                <ChevronDown
+                className="absolute right-2 top-1/2 -translate-y-1/2 pointer-events-none"
+                size={17}
+                />
+               </div>
+  
+
+
+
+</div>
 
 
         </>
