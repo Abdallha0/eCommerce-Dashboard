d1c13ce49e7146a4db078d0c5d7bde3d41205895
Mariam-1-m <mariumaabdelrahman7@gmail.com>
2026-07-15 16:52:11 +0300

    Updated setting page

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import SettingSec1 from '../components/settings/sec1'
+import ThemeButton from '../components/settings/ThemeButton'
+
 function SettingsPage() {
   return (
-    <div className='flex justify-center items-center p-8'>
+    <div className='flex flex-col  justify-center items-center p-8'>
       <SettingSec1/>
+      <ThemeButton/>
     </div>
   )
 }
