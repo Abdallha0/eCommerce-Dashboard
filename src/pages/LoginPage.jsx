38e2af698d9a11ef0126ea7d962036e9fee3a993
Mariam-1-m <mariumaabdelrahman7@gmail.com>
2026-07-17 17:29:06 +0300

    some edits in layout

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,8 +4,8 @@ import React from "react";
 
 function LoginPage() {
   return (
-    <div className="min-h-screen overflow-hidden bg-linear-to-br from-slate-100 via-white to-slate-200 dark:from-slate-950 dark:via-slate-900 dark:to-black flex items-center justify-center px-4 py-8">
-      <div className="relative z-10 w-full max-w-6xl">
+    <div className="h-150 overflow-hidden bg-linear-to-br m-5 from-slate-100 via-white to-slate-200 dark:from-slate-950 dark:via-slate-900 dark:to-black flex items-center justify-center px-4 py-8">
+      <div className="relative z-10 w-full max-w-5xl">
         <div className="grid overflow-hidden rounded-4xl border border-slate-200 dark:border-slate-800 bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl shadow-2xl lg:grid-cols-2">
           <LoginSidePanel />
 
